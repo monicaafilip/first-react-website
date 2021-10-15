a57f73cbec7d039e23e1aec781ff9d2099d92cd7
Monica <32903674+monicaafilip@users.noreply.github.com>
2021-10-15 18:21:53 +0300

    finishing checkout page + but still having a problem with the arrows from increasing/decreasing the wauntity

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,4 +12,19 @@ export const addItemToCart = (cartItems, newItem) => {
     }
 
     return [ ...cartItems, {...newItem, quantity: 1}]
-}
+};
+
+export const removeItemFromCart = (cartItems, item) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === item.id);
+
+    if (existingCartItem.quantity === 1)
+    {
+        return cartItems.filter(cartItem => cartItem.id !== item.id);
+    }
+
+    return cartItems.map(
+        cartItem => cartItem.id === item.id
+        ? {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    );
+};
