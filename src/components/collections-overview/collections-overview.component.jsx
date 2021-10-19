1d6135a8300727b7b9ac628497ae77dd3ef98dfb
Monica <32903674+monicaafilip@users.noreply.github.com>
2021-10-19 12:14:49 +0300

    added data normalization + collection page (where I have a bug, so that the collection passed as argument is undefined, so we cannot get the title and the items for the page)

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CollectionsPreview from '../collection-preview/collection-preview.component';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
 import { selectCollections } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
@@ -10,9 +11,9 @@ import './collections-overview.styles.scss';
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
     {
-        collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionsPreview key={id} {...otherCollectionProps} />
-        ))
+        collections.map( ({id, ...otherProps}) => (
+            <CollectionPreview key={id} {...otherProps}/>
+          ))
     }
   </div>
 );
